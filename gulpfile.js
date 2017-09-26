d1715a1e5ffcb1844a51705d3ee53d4b183c5fea
Leonard <leonard.horne@gmail.com>
2017-09-26 16:37:17 -0400

    Changed themes.

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ var header = require('gulp-header');
 var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
-var filter = require('gulp-filter');
 var pkg = require('./package.json');
 
 // Set the banner content
@@ -19,7 +18,7 @@ var banner = ['/*!\n',
 
 // Compiles SCSS files from /scss into /css
 gulp.task('sass', function() {
-  return gulp.src('scss/freelancer.scss')
+  return gulp.src('scss/new-age.scss')
     .pipe(sass())
     .pipe(header(banner, {
       pkg: pkg
@@ -32,7 +31,7 @@ gulp.task('sass', function() {
 
 // Minify compiled CSS
 gulp.task('minify-css', ['sass'], function() {
-  return gulp.src('css/freelancer.css')
+  return gulp.src('css/new-age.css')
     .pipe(cleanCSS({
       compatibility: 'ie8'
     }))
@@ -47,7 +46,7 @@ gulp.task('minify-css', ['sass'], function() {
 
 // Minify custom JS
 gulp.task('minify-js', function() {
-  return gulp.src('js/freelancer.js')
+  return gulp.src('js/new-age.js')
     .pipe(uglify())
     .pipe(header(banner, {
       pkg: pkg
@@ -80,6 +79,10 @@ gulp.task('copy', function() {
 
   gulp.src(['node_modules/jquery.easing/*.js'])
     .pipe(gulp.dest('vendor/jquery-easing'))
+
+  gulp.src(['node_modules/simple-line-icons/*/*'])
+    .pipe(gulp.dest('vendor/simple-line-icons'))
+
 
   gulp.src([
       'node_modules/font-awesome/**',
